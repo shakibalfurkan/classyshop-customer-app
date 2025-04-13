7d6f824d6e319a155ccaf4389f09b8cf191bddec
shakib2123 <mshakibalhasan21@gmail.com>
2025-04-13 21:27:46 +0600

    category collapse section created

--- src/components/shared/Header/CategorySidebar/CategorySidebar.tsx
+++ src/components/shared/Header/CategorySidebar/CategorySidebar.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import {
   Sheet,
   SheetContent,
@@ -5,10 +11,30 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { FiMinusSquare, FiPlusSquare } from "react-icons/fi";
+
+import { useState } from "react";
+
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { RiMenu2Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 export default function CategorySidebar() {
+  const [openMenus, setOpenMenus] = useState<number[]>([]);
+  const [openSubMenus, setOpenSubMenus] = useState<number[]>([]);
+
+  const toggleMenu = (id: number) => {
+    setOpenMenus((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
+
+  const toggleSubMenu = (id: number) => {
+    setOpenSubMenus((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
+
   return (
     <section>
       <Sheet>
@@ -25,8 +51,206 @@ export default function CategorySidebar() {
           <SheetHeader className="border-b border-gray-200">
             <SheetTitle>Shop By Categories</SheetTitle>
           </SheetHeader>
-          <div className="grid gap-4 py-1 px-4">
-            <p>hello world</p>
+          <div className="px-4">
+            {/* menu */}
+            <div className="">
+              <div className="flex items-center justify-between w-full">
+                <Link
+                  to="#"
+                  className="text-sm hover:text-primary transition-colors duration-300 ease-out w-full py-2"
+                >
+                  Fashion
+                </Link>
+                <Button
+                  onClick={() => toggleMenu(1)}
+                  variant="ghost"
+                  size="icon"
+                  className="cursor-pointer border-none outline-none"
+                >
+                  {openMenus.includes(1) ? <FiMinusSquare /> : <FiPlusSquare />}
+                </Button>
+              </div>
+
+              <div
+                className={`w-full basis-full overflow-hidden transition-all duration-300 ease-in-out ${
+                  openMenus.includes(1)
+                    ? "max-h-[600px] opacity-100 mt-1"
+                    : "max-h-0 opacity-0"
+                }`}
+              >
+                <div className="relative mx-auto flex w-full flex-col pl-4">
+                  {/* sub menu */}
+                  <div className="">
+                    <div className="flex items-center justify-between w-full">
+                      <Link
+                        to="#"
+                        className="text-sm hover:text-primary transition-colors duration-300 ease-out w-full py-2"
+                      >
+                        Apparel
+                      </Link>
+                      <Button
+                        onClick={() => toggleSubMenu(1)}
+                        variant="ghost"
+                        size="icon"
+                        className="cursor-pointer border-none outline-none"
+                      >
+                        {openSubMenus.includes(1) ? (
+                          <FiMinusSquare />
+                        ) : (
+                          <FiPlusSquare />
+                        )}
+                      </Button>
+                    </div>
+
+                    <div
+                      className={`w-full basis-full overflow-hidden transition-all duration-300 ease-in-out ${
+                        openSubMenus.includes(1)
+                          ? "max-h-[600px] opacity-100 mt-1"
+                          : "max-h-0 opacity-0"
+                      }`}
+                    >
+                      <div className="relative flex w-full flex-col pl-4 gap-1">
+                        <p className="text-sm">Rolling Diamond</p>
+                        <p className="text-sm">Rolling Diamond</p>
+                        <p className="text-sm">Rolling Diamond</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="flex items-center justify-between w-full">
+                <Link
+                  to="#"
+                  className="text-sm hover:text-primary transition-colors duration-300 ease-out w-full py-2"
+                >
+                  Fashion
+                </Link>
+                <Button
+                  onClick={() => toggleMenu(2)}
+                  variant="ghost"
+                  size="icon"
+                  className="cursor-pointer border-none outline-none"
+                >
+                  {openMenus.includes(2) ? <FiMinusSquare /> : <FiPlusSquare />}
+                </Button>
+              </div>
+
+              <div
+                className={`w-full basis-full overflow-hidden transition-all duration-300 ease-in-out ${
+                  openMenus.includes(2)
+                    ? "max-h-[600px] opacity-100 mt-1"
+                    : "max-h-0 opacity-0"
+                }`}
+              >
+                <div className="relative mx-auto flex w-full flex-col pl-4">
+                  {/* sub menu */}
+                  <div className="">
+                    <div className="flex items-center justify-between w-full">
+                      <Link
+                        to="#"
+                        className="text-sm hover:text-primary transition-colors duration-300 ease-out w-full py-2"
+                      >
+                        Apparel
+                      </Link>
+                      <Button
+                        onClick={() => toggleSubMenu(2)}
+                        variant="ghost"
+                        size="icon"
+                        className="cursor-pointer border-none outline-none"
+                      >
+                        {openSubMenus.includes(2) ? (
+                          <FiMinusSquare />
+                        ) : (
+                          <FiPlusSquare />
+                        )}
+                      </Button>
+                    </div>
+
+                    <div
+                      className={`w-full basis-full overflow-hidden transition-all duration-300 ease-in-out ${
+                        openSubMenus.includes(2)
+                          ? "max-h-[600px] opacity-100 mt-1"
+                          : "max-h-0 opacity-0"
+                      }`}
+                    >
+                      <div className="relative flex w-full flex-col pl-4 gap-1">
+                        <p className="text-sm">Rolling Diamond</p>
+                        <p className="text-sm">Rolling Diamond</p>
+                        <p className="text-sm">Rolling Diamond</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="flex items-center justify-between w-full">
+                <Link
+                  to="#"
+                  className="text-sm hover:text-primary transition-colors duration-300 ease-out w-full py-2"
+                >
+                  Fashion
+                </Link>
+                <Button
+                  onClick={() => toggleMenu(3)}
+                  variant="ghost"
+                  size="icon"
+                  className="cursor-pointer border-none outline-none"
+                >
+                  {openMenus.includes(3) ? <FiMinusSquare /> : <FiPlusSquare />}
+                </Button>
+              </div>
+
+              <div
+                className={`w-full basis-full overflow-hidden transition-all duration-300 ease-in-out ${
+                  openMenus.includes(3)
+                    ? "max-h-[600px] opacity-100 mt-1"
+                    : "max-h-0 opacity-0"
+                }`}
+              >
+                <div className="relative mx-auto flex w-full flex-col pl-4">
+                  {/* sub menu */}
+                  <div className="">
+                    <div className="flex items-center justify-between w-full">
+                      <Link
+                        to="#"
+                        className="text-sm hover:text-primary transition-colors duration-300 ease-out w-full py-2"
+                      >
+                        Apparel
+                      </Link>
+                      <Button
+                        onClick={() => toggleSubMenu(3)}
+                        variant="ghost"
+                        size="icon"
+                        className="cursor-pointer border-none outline-none"
+                      >
+                        {openSubMenus.includes(3) ? (
+                          <FiMinusSquare />
+                        ) : (
+                          <FiPlusSquare />
+                        )}
+                      </Button>
+                    </div>
+
+                    <div
+                      className={`w-full basis-full overflow-hidden transition-all duration-300 ease-in-out ${
+                        openSubMenus.includes(3)
+                          ? "max-h-[600px] opacity-100 mt-1"
+                          : "max-h-0 opacity-0"
+                      }`}
+                    >
+                      <div className="relative flex w-full flex-col pl-4 gap-1">
+                        <p className="text-sm">Rolling Diamond</p>
+                        <p className="text-sm">Rolling Diamond</p>
+                        <p className="text-sm">Rolling Diamond</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </SheetContent>
       </Sheet>
