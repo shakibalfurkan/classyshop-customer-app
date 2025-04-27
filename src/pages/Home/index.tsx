0d1ebb74ccabc580847b43142ece558d23e017b0
shakib2123 <mshakibalhasan21@gmail.com>
2025-04-27 21:28:05 +0600

    Featured products section with slider has been created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import DeliveryNoticeBar from "@/components/Home/DeliveryNoticeBar";
 import ExclusiveDeals from "@/components/Home/ExclusiveDeals/ExclusiveDeals";
 import FeaturedCategories from "@/components/Home/FeaturedCategories";
 import FeaturedOffers from "@/components/Home/FeaturedOffers";
+import FeaturedProducts from "@/components/Home/FeaturedProducts/FeaturedProducts";
 import LatestProducts from "@/components/Home/LatestProducts/LatestProducts";
 import PopularProducts from "@/components/Home/PopularProducts";
 import ProductHighlightBanner from "@/components/Home/ProductHighlightBanner";
@@ -19,6 +20,7 @@ export default function Home() {
       <PopularProducts />
       <ProductHighlightBanner />
       <LatestProducts />
+      <FeaturedProducts />
       <ExclusiveDeals />
     </section>
   );
