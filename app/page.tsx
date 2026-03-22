44696887f87b7f77382a2fb1e3bda143681e284a
shakibalfurkan <shakibalfurkan@gmail.com>
2026-03-22 07:33:00 +0600

    theme setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function page() {
   return (
     <section>
-      <h1 className="text-3xl font-bold">This is page component</h1>
+      <h1 className="text-3xl font-bold p-4 m-4 shadow-2xl text-primary bg-trust-bg">
+        This is page component
+      </h1>
     </section>
   );
 }
